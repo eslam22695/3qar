05865eed99f0900b54da3846ea9f160303df9071
MAHMOUDABASS206 <59824224+MAHMOUDABASS206@users.noreply.github.com>
2020-04-18 22:59:30 +0200

    districtController

---
+++ app/Http/Controllers/admin/DistrictController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\City;
+use App\District;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DistrictController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $district = District::where('status',1)->get();
+        $cities = City::where('status',1)->get();
+        return view('admin.district.index',compact('district','cities'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate(request(),
+            [
+                'name'        => 'required',
+
+            ]);
+
+
+        $input = $request->all();
+
+        District::create($input);
+        Session::flash('success','تم الاضافه بنجاح');
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate(request(),
+            [
+                'name'    => 'required',
+
+            ]);
+
+        $input = $request->all();
+        if($district = District::find($id)){
+
+            $district->update($input);
+            Session::flash('success','تم التعديل بنجاح');
+            return redirect()->back();
+        }else{
+            Session::flash('danger','لم يتم التعديل ');
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $delete =  District::find($id);
+        $delete->delete();
+        session()->flash('success','تم الحذف بنجاح');
+        return back();
+    }
+}
