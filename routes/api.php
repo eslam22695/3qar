98b68f705940da9061024b3a4af4c915a92e7ab3
eslam22695 <eslamosama22695@gmail.com>
2020-05-15 03:51:00 +0200

    API V1.1

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::get('contact', 'api\IndexController@contact');
 Route::post('contact_form', 'api\IndexController@contact_form');
 Route::get('services', 'api\IndexController@services');
 Route::post('service_request', 'api\IndexController@service_request');
+Route::get('blog', 'api\IndexController@blog');
+Route::get('blog_details/{id}', 'api\IndexController@blog_details');
+
 
 
 /* Route::group(['middleware' => 'auth:api'], function(){
